c3789921b9785bfe5e4db1d3e83fba9aea2de01a
Max Aldo <maxsogbossi@gmail.com>
2026-02-27 13:20:17 +0100

    feat: mise en place du state global et connexion de la liste dans App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TransactionList from "./components/TransactionList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [transactions, setTransactions] = useState([]);
+
+  const deleteTransaction = (id) => {
+    setTransactions(transactions.filter(t => t.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Budget Tracker</h1>
+
+      <TransactionList
+        transactions={transactions}
+        onDelete={deleteTransaction}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
